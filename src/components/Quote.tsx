4668c85c61130d0189516b25f189f08f9c2cd3ef
Leonardo Nicolas Bigentini <leniu2022@gmail.com>
2024-09-29 20:15:44 -0400

    fixed quote generator api

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -5,21 +5,64 @@ import { useState, useEffect } from "react";
 const Quote = () => {
   const [quotes, setQuotes] = useState([]);
   const [quote, setQuote] = useState<{ text: string; author: string } | null>(null);
+  // useEffect(() => {
+  //   fetch("https://zenquotes.io/api/quotes") //"https://type.fit/api/quotes"
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setQuotes(json);
+  //       setQuote(json[0]);
+  //     });
+  // }, []);
+
+
+  // Old quote api stopped working, so using a new API
   useEffect(() => {
-    fetch("https://type.fit/api/quotes")
+    fetchNewQuote();
+  }, []);
+
+  const fetchNewQuote = () => {
+    fetch("http://api.quotable.io/random")
       .then((res) => res.json())
       .then((json) => {
-        setQuotes(json);
-        setQuote(json[0]);
+        setQuote({
+          text: json.content,
+          author: json.author,
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching quotes:", error);
       });
-  }, []);
-  const newQuote = () => {
-    let temp = Math.floor(Math.random() * quotes.length);
-    setQuote(quotes[temp]);
   };
+  
+
+  
+
+
+  // useEffect(() => {
+  //   fetch("https://zenquotes.io/api/quotes") // Or "https://type.fit/api/quotes"
+  //     .then((res) => {
+  //       if (!res.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return res.json();
+  //     })
+  //     .then((json) => {
+  //       setQuotes(json);
+  //       setQuote(json[0]);
+  //     })
+  //     .catch((error) => {
+  //       console.log("Error fetching quotes:", error);
+  //     });
+  // }, []);
+
+  
+  // const newQuote = () => {
+  //   let temp = Math.floor(Math.random() * quotes.length);
+  //   setQuote(quotes[temp]);
+  // };
   return (
     <div className="container2">
-      <button className="button2" onClick={newQuote}>
+      <button className="button2" onClick={fetchNewQuote}> 
         New Quote
       </button>
       <h2>{quote?.text}</h2>
